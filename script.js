809a71ffeed36f538763275b5fc4323f5e50b8a7
TatianeMarinho <devtatianemarinho@gmail.com>
2023-04-15 11:06:05 -0400

    enviando o primeiro

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 
-
+// 1 - Adicione à página o título "Paleta de Cores"
 const tagH1 = () => {
-
   const title = document.createElement('h1');
-  title.id = ' title';
+  title.id = 'title';
   title.innerText = 'Paleta de Cores';
   document.body.appendChild(title);
 };
 tagH1();
+
+
