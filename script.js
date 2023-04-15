4eb3b1735d7a91c9aa2156a16d49803e0ec7628b
TatianeMarinho <devtatianemarinho@gmail.com>
2023-04-15 10:47:11 -0400

    enviando o primeiro

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+const tagH1 = () => {
+
+  const title = document.createElement('h1');
+  title.id = ' title';
+  title.innerText = 'Paleta de Cores';
+  document.body.appendChild(title);
+};
+tagH1();
